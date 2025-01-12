390b0504413c996edf57db4b3a8439708951163d
abhi19751 <153172902+abhi19751@users.noreply.github.com>
2025-01-12 11:50:54 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,39 @@
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function(eleventyConfig) {
-  // Additional configurations can be added here
+  // Add data extensions
+  eleventyConfig.addDataExtension("json", contents => JSON.parse(contents));
+
+  // Passthrough copy for assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+  
+  // Add sitemap plugin
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://example.com",
+    },
+  });
+
+  // Add filter for finding translations
+  eleventyConfig.addFilter("translate", function(key, locale) {
+    try {
+      return locale[key] || key;
+    } catch (e) {
+      return key;
+    }
+  });
+
+  // Return 11ty config object
   return {
     dir: {
       input: "src",
-      output: "_site"
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
     },
-    templateFormats: ["njk"],
-    passthroughFileCopy: true,
-    markdownTemplateEngine: "njk"
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true
   };
 };
