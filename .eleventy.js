9485646e2cec68a442204c6b4b00fe75f1756196
abhi19751 <153172902+abhi19751@users.noreply.github.com>
2025-01-12 10:30:05 +0530

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addDataExtension("json", contents => JSON.parse(contents));
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://example.com", // Replace with your actual domain
+    },
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    },
+    templateFormats: ["njk"],
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "njk",
+  };
+};
