b9c4ec48b3ebae8bcf4696125a676cd2c0d46645
abhi19751 <153172902+abhi19751@users.noreply.github.com>
2025-01-12 11:08:00 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addDataExtension("json", contents => JSON.parse(contents));
-
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://example.com",
+    },
+  });
 
   return {
     dir: {
